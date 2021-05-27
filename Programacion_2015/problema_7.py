869b6adfb1880e65e46b121a6f3c51b440e11042
Giovanni <giovannilopez9808@gmail.com>
2021-05-26 20:42:12 -0500

    Clean

--- Programacion_2015/problema_7.py
+++ Programacion_2015/problema_7.py
@@ -1,13 +1,10 @@
 def subset_sum(numbers, target, numbers_partial=[]):
     s = sum(numbers_partial)
-
-    # check if the partial sum is equals to sum_data
     if s == sum_data:
         print("sum(%s)=%s" % (numbers_partial,
                               sum_data))
     if s >= sum_data:
-        return   # if we reach the number why bother to continue
-
+        return
     for i in range(len(numbers)):
         n = numbers[i]
         remaining = numbers[i+1:]
@@ -25,5 +22,4 @@ data = [1,
         6,
         7]
 sum_data = 7
-count = subset_sum(data, sum_data)
-print(count)
+subset_sum(data, sum_data)
